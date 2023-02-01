516466c9ff123d7fcf4fa8ad58ccc39f8782cdbc
eduardomanzini <eduardo_manzini@hotmail.com>
2023-02-01 11:50:32 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
+
 import com.nelioalves.workshopmongo.repository.UserRepository;
 
 @Service
